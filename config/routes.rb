4d7382d7a1effadaf0d3d6cb73671fcdbdfc8747
gkillilick <gkillick@gmail.com>
2009-12-18 16:56:56 -0700

    added some css but have to go another way with coding the layout so just temperary

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
 map.resources :activities
 map.resources :sports
+map.resources :user_sessions
+map.resources :users
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091215010141) do
+ActiveRecord::Schema.define(:version => 20091218001952) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(:version => 20091215010141) do
 
   create_table "sports", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",               :null => false
+    t.string   "crypted_password",    :null => false
+    t.string   "password_salt",       :null => false
+    t.string   "persistence_token",   :null => false
+    t.string   "single_access_token", :null => false
+    t.string   "perishable_token",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
