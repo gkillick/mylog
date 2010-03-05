1009b363b173d27cc933b838b7a0378a44ce58fd
Andres Mejia <amejia@mentezt61.(none)>
2010-03-04 20:53:26 -0500

    Adding homework

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -1,8 +1,11 @@
 class LogEntriesController < ApplicationController
+  before_filter :require_athlete
+
   def new
     @athlete = current_user
     @log_entry = @athlete.log_entries.build
   end
+
   def create
     @athlete = current_user
     @log_entry = @athlete.log_entries.build(params[:log_entry])
@@ -12,6 +15,7 @@ class LogEntriesController < ApplicationController
       render :action=>"new"
     end
   end
+
   def index 
     @athlete = current_user
     @log_entries = @athlete.log_entries.all 
