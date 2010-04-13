7fa0062e2492d3e3d147ec44257bdcc4ce456dbf
Andrés Mejía <andmej@gmail.com>
2010-04-12 22:46:15 -0500

    Adding HH:MM duration for Workouts, with tests

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,17 @@ class Workout < ActiveRecord::Base
   belongs_to :log_entry
   validates_numericality_of :duration
   validates_presence_of :duration, :time_of_day, :activity_id
+
+  def duration=(something)
+    if something =~ /\A\d{1,2}:\d{2,2}\z/
+      hours, minutes = something.split(":") 
+      self[:duration] = hours.to_i * 60 + minutes.to_i
+    else
+      self[:duration] = something.to_i * 60
+    end
+  end
+  
+  def duration
+    "%d:%.2d" % [self[:duration] / 60, self[:duration] % 60]
+  end
 end
---
+++ spec/models/workout_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Workout do
+  before(:each) do
+    @valid_attributes = {
+      :duration => 3,
+      :activity_id => 1,
+      :time_of_day => "AM"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Workout.create!(@valid_attributes)
+  end
+
+
+  it "should convert durations in HH:MM format to minutes and store it like that to the database" do
+    w = Workout.create(@valid_attributes)
+    w.duration = "2:30"
+    w[:duration].should == 150
+  end
+
+  it "should assume that durations consisting of a single number are hours" do
+    w = Workout.create(@valid_attributes)
+    w.duration = "2"
+    w[:duration].should == 120
+  end
+
+  it "should convert durations stored in the database back to HH:MM format" do
+    w = Workout.create(@valid_attributes)
+    w[:duration] = 150
+    w.duration == "2:30"
+  end
+end
