a50bd3482e322b667555e90ceea0515e5621ea22
gkillilick <gkillick@gmail.com>
2010-04-15 19:08:31 -0600

    added Plus button

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-	$("#log_entry_feel_slider").slider({min:1, max:10});
+	$("#log_entry_feel_slider").slider({min:0, max:100});
 	$('#log_entry_feel_slider').bind('slidechange', function(event, ui) {
 		$("#log_entry_feel").val($("#log_entry_feel_slider").slider('value'));
 	});
-	$("#log_entry_feel_slider").slider('value', 5);
+	$("#log_entry_feel_slider").slider('value', 50);
 });
