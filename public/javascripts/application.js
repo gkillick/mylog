30961ce59e9d9b0c9b7d25eddf4ba45752a742f2
Andrés Mejía <andmej@gmail.com>
2010-04-20 19:07:26 -0500

    Fixing bug with slider

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
 	$('#log_entry_feel_slider').bind('slidechange', function(event, ui) {
 		$("#log_entry_feel").val($("#log_entry_feel_slider").slider('value'));
 	});
-	$("#log_entry_feel_slider").slider('value', 50);
+	if ($("#log_entry_feel").val() != ""){
+    	$("#log_entry_feel_slider").slider('value', $("#log_entry_feel").val());				
+	}else{
+    	$("#log_entry_feel_slider").slider('value', 50);		
+	}
 	
 	$("input.duration_field").live("click", function(event){
 		if ($(this).val() == "0:00"){
